6072f03955e2a0d54d2567e41f23a2039c8c5b06
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-04-12 01:48:48 +0600

    OptionalDemo03 using Optional.ofNullable(T)

---
+++ optional/optionalDemo/src/com/farhadTech/optional/OptionalDemo03.java
@@ -0,0 +1,25 @@
+package com.farhadTech.optional;
+
+import java.util.Optional;
+import java.util.Random;
+
+public class OptionalDemo03 {
+    public static void main(String[] args) {
+        Optional<String> optionalMsg = sayHello();
+        if (optionalMsg.isPresent()) { // if (!optionalMsg.isEmpty())
+            System.out.println(optionalMsg.get().toUpperCase());
+        }
+    }
+    public static Optional<String> sayHello() {
+        int num = new Random().nextInt(10);
+        System.out.println("Random number is " + num);
+        String msg = null;
+        if( num % 2 == 0 ) {
+            msg = "Hello World";
+        }
+        return Optional.ofNullable(msg); // if the value is present returns the the value, otherwise returns NullPointerException.
+    }
+}
+/**
+Note: Using Optional we are not getting rid of NullPointerExceptions. Using Optional we are handling the NullPointerException gracefully.
+ **/
